1be0ead692e4ea1a4b315ce1cb18c86fd53263d8
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-01-09 08:07:10 +0300

    styling

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,1 +1,3 @@
-
+<div style="width: 100% ;  height : 600px; background-color: indianred ; text-align: center">
+    <p style="font-weight: bolder ; font-size: 23px ; padding-top: 300px"> This is the banner</p>
+</div>
